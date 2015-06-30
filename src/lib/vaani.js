c04ad7bc0bfa7b2cf2ab1f9a8ccf0b5fbd190247
jedireza <reza@akhavan.me>
2015-06-29 23:22:35 -0700

    updated vaani class docs/comments

--- src/lib/vaani.js
+++ src/lib/vaani.js
@@ -2,11 +2,12 @@
 
 class Vaani {
   /**
-   * Constructs a new instance
-   *
-   * @param options An options object
-   * @param options.grammar The grammar list used by the speech recognition
-   * @param options.interpreter The function to call after speech recogition is attempted.
+   * @constructor
+   * @param options {Object}
+   * @param options.grammar {String} The JSGF 1.0 grammar list to be
+   *        used by the speech recognition library
+   * @param options.interpreter {Function} The function to call after
+   *        speech recogition is attempted
    */
   constructor (options = {}) {
     if (!options.hasOwnProperty('grammar')) {
@@ -25,58 +26,57 @@ class Vaani {
   }
 
   /**
-   * Say a sentence to the user and optionally wait for a response.
-   *
-   * @param sentence The sentence to be spoken.
-   * @param isWaitingForCommandResponse Indicates we will wait for a response
-   *                                    after the sentence has been said.
+   * Says a sentence and optionally wait for a response
+   * @param sentence {String} The sentence to be spoken
+   * @param waitForResponse {Boolean} Indicates we will wait for a
+   *        response after the sentence has been said
    */
-  say (sentence, isWaitingForCommandResponse) {
-    if (isWaitingForCommandResponse) {
+  say (sentence, waitForResponse) {
+    if (waitForResponse) {
       this._interpretingCommand = true;
     }
 
-    var lang = 'en'; // XXX: should be detected based on system language
+    var lang = 'en'; // Aus: should be detected based on system language
     var sayItProud;
 
-    // use built-in speech synthesis
-    if (navigator.userAgent.indexOf('Mobile') > -1) {
-      sayItProud = () => {
-        var speechSynthesisUtterance = new SpeechSynthesisUtterance(sentence);
-
-        speechSynthesisUtterance.lang = lang;
-        speechSynthesisUtterance.addEventListener('end', () => {
-          if (isWaitingForCommandResponse) {
-            this.listen();
-          }
-        });
-
-        speechSynthesis.speak(speechSynthesisUtterance);
-      };
-    }
-    // XXX: To be removed. This is a temporary solution to help dev in the browser.
-    else {
+    // Reza: This is a temporary solution to help dev in the browser
+    if (!navigator.userAgent.indexOf('Mobile') > -1) {
       sayItProud = () => {
         var audio = document.createElement('audio');
-        var url = 'http://speechan.cloudapp.net/weblayer/synth.ashx?lng=' + lang;
+        var url = 'http://speechan.cloudapp.net/weblayer/synth.ashx';
+            url += '?lng=' + lang;
             url += '&msg=' + sentence;
 
         audio.src = url;
         audio.setAttribute('autoplay', 'true');
         audio.addEventListener('ended', () => {
-          if (isWaitingForCommandResponse) {
+          if (waitForResponse) {
             this.listen();
           }
         });
       };
     }
+    else {
+      sayItProud = () => {
+        var utterance = new SpeechSynthesisUtterance(sentence);
 
-    // Wait an extra 100ms for the audio output to stabilize off.
+        utterance.lang = lang;
+        utterance.addEventListener('end', () => {
+          if (waitForResponse) {
+            this.listen();
+          }
+        });
+
+        speechSynthesis.speak(utterance);
+      };
+    }
+
+    // Aus: Wait an extra 100ms for the audio output to stabilize off
     setTimeout(sayItProud.bind(this), 100);
   }
 
   /**
-   * Listen for a response from the user.
+   * Listen for a response from the user
    */
   listen () {
     this.speechRecognition.start();
@@ -94,27 +94,29 @@ class Vaani {
         if (event.results[i].isFinal) {
           isFinal = true;
           transcript += event.results[i][0].transcript;
-          // XXXAus: This is useless right now but the idea is we wouldn't
-          //         always complete the action or command requested if the
-          //         confidence level is too low.
+          // Aus: This is useless right now but the idea is we wouldn't
+          // always complete the action or command requested if the
+          // confidence level is too low
           confidence = event.results[i][0].confidence;
         }
         else {
           partialTranscript += event.results[i][0].transcript;
-          // XXXAus: In theory, partial transcripts shouldn't be used as their
-          //         confidence will always be lower than a final transcript.
-          //         We should ask the user to repeat what they want when all
-          //         we have is a partial transcript with 'low' confidence.
+          // Aus: In theory, partial transcripts shouldn't be used
+          // as their confidence will always be lower than a final
+          // transcript. We should ask the user to repeat what they
+          // want when all we have is a partial transcript with 'low'
+          // confidence.
+
           confidence = event.results[i][0].confidence;
         }
       }
 
-      // XXXAus: We'll fall back to the partial transcript if there isn't a
-      //         final one for now. It actually looks like we never get a
-      //         final transcript currently.
+      // Aus: We'll fall back to the partial transcript if there
+      // isn't a final one for now. It actually looks like we never
+      // get a final transcript currently.
       var usableTranscript = transcript || partialTranscript;
 
-      // XXXAus: Ugh. This is really crappy error handling.
+      // Aus: Ugh. This is really crappy error handling
       if (usableTranscript == "ERROR") {
         var getOffMyLawn = new Error('Unrecognized speech.');
         this._interpreter(getOffMyLawn);
